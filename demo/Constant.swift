c1034851e7149ff0e928be1888e145a7e4ceb596
Kriti Agarwal <kriti.agarwal4709@gmail.com>
2019-12-09 01:15:56 +0530

    final commit

---
+++ demo/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  PartTimeEnterprise
+//
+//  Created by Techugo on 09/07/19.
+//  Copyright © 2019 Techugo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+    let R = 6378137.0
+    let dn = 0.0
+    let de = -50.0
+    let pi = Double.pi
+
+
